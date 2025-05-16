233d2f6162e6426bfc3f4907d42b419f3cddce1d
unknown <shahankari98@gmail.com>
2025-05-15 23:05:11 -0700

    fifth commt

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,31 +1,56 @@
 var db = require('../config/connection')
 var collection = require('../config/collections')
+const { response } = require('express')
 var objectId = require('mongodb').ObjectID
 
-module.exports={
-    addProduct:(product,callback)=>{
+module.exports = {
+    addProduct: (product, callback) => {
         console.log(product)
-        db.get().collection('product').insertOne(product).then((data)=>{
-            
-          
+        db.get().collection('product').insertOne(product).then((data) => {
+
+
             callback(data.insertedId)
 
         })
     },
-    getAllProducts:()=>{
-        
-        return new Promise(async(resolve,reject)=>{
+    getAllProducts: () => {
+
+        return new Promise(async (resolve, reject) => {
             let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
-            resolve (products)
+            resolve(products)
         })
     },
-    deleteProduct: (proId)=>{
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(proId)}).then((response)=>{
+    deleteProduct: (proId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: objectId(proId) }).then((response) => {
                 console.log(response);
-                
+
                 resolve(response)
             })
         })
+    },
+    getProductDetails: (proId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({ _id: objectId(proId) }).then((product) => {
+                resolve(product)
+            })
+        })
+    },
+    updateProduct: (proId, proDetails) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({ _id: objectId(proId) },
+            {
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Category:proDetails.Category,
+                    Price:proDetails.Price,
+
+                }
+            }).then((response)=>{
+                resolve(response)
+            })
+        })
+
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ var productHelpers = require('../helpers/product-helpers');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   productHelpers.getAllProducts().then((products) => {
-    console.log(products);
+
     res.render('admin/view-products', { products, admin: true });
 
   })
@@ -17,12 +17,11 @@ router.get('/add-product', function (req, res) {
   res.render('admin/add-product')
 })
 router.post('/add-product', (req, res) => {
-  console.log(req.body)
-  console.log(req.files.image)
+
 
   productHelpers.addProduct(req.body, (insertedId) => {
     let image = req.files.image
-    console.log(insertedId)
+
     image.mv('./public/product-images/' + insertedId + '.jpg', (err, done) => {
       if (!err) {
         res.render('admin/add-product')
@@ -35,10 +34,28 @@ router.post('/add-product', (req, res) => {
   })
 })
 
-router.get('/delete-product/:id',(req,res)=>{
-  let proId=req.params.id
-  productHelpers.deleteProduct(proId).then((response)=>{
-    res.redirect('/admin/')
+router.get('/delete-product/:id', (req, res) => {
+  let proId = req.params.id
+  productHelpers.deleteProduct(proId).then((response) => {
+    res.redirect('admin/')
+  })
+})
+
+router.get('/edit-product/:id', async (req, res) => {
+  let product = await productHelpers.getProductDetails(req.params.id)
+  console.log(product);
+
+  res.render('admin/edit-product', { product })
+})
+
+router.post('/edit-product/:id', (req, res) => {
+  let insertedId = req.params.id
+  productHelpers.updateProduct(req.params.id, req.body).then(() => {
+    res.redirect('/admin')
+    if (req.files.image) {
+      let image = req.files.image
+      image.mv('./public/product-images/' + insertedId + '.jpg')
+    }
   })
 })
 
